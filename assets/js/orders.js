f4a60e094415c6de9a8a89866d1faa47d136f8e2
Oummoul <Oummy20180@gmail.com>
2022-06-26 17:10:24 +0200

    admin dashboard

---
+++ assets/js/orders.js
@@ -0,0 +1,52 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        PaymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        PaymentStatus: 'Refunded',
+        Shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        PaymentStatus: 'Paid',
+        Shipping: 'Delivered'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        PaymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        PaymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        PaymentStatus: 'Paid',
+        Shipping: 'Delivered'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        PaymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+    // {
+    //     productName: 'Foldable Mini Drone',
+    //     productNumber: '85631',
+    //     PaymentStatus: 'Due',
+    //     Shipping: 'Pending'
+    // },
+    
+    
+]
